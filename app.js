285f6f1d9c056c709828746ddf347aef43e308cc
Alba <34134103+koalba@users.noreply.github.com>
2022-03-17 16:54:37 +0100

    Update

--- app.js
+++ app.js
@@ -1,56 +1,39 @@
 'use strict'
 
+const d = document
+
 function checkNumber () {
-    let inputValue = document.querySelector('.main__input').value
+
+    const inputValue = d.querySelector('.main__input').value
 
     if(inputValue !== ""){
-        isItEven(inputValue)
-        threeMultiple(inputValue)
-        sevenMultiple(inputValue)
 
-        console.log("The number " + inputValue + " is:")
-        console.log("   - " + isItEven(inputValue))
-        console.log("   - " + threeMultiple(inputValue))
-        console.log("   - " + sevenMultiple(inputValue))
+        calcMultiple( inputValue, 2 )
+        calcMultiple( inputValue, 3 )
+        calcMultiple( inputValue, 7 )
+        
     }
 
 }
 
-function isItEven(number) {
-    let node = document.querySelector('.even')
-    const calc = number % 2
+function calcMultiple( number, multiple ){
 
-    if(calc !== 0){
-        node.classList.remove('active')
-        return "NOT EVEN"
-    } else{
-        node.classList.add('active')
-        return "EVEN"
+    const calc = number % multiple
+
+    let node
+
+    switch (multiple) {
+        case 2:
+        node = d.querySelector('.even')
+            break
+        case 3:
+        node = d.querySelector('.multThree')
+            break
+        case 7:
+        node = d.querySelector('.multSeven')
+            break
     }
-}
-
-function threeMultiple(number) {
-    let node = document.querySelector('.multThree')
-    const calc = number % 3
-
-    if(calc !== 0){
-        node.classList.remove('active')
-        return "NOT MULTIPLE OF 3"
-    } else{
-        node.classList.add('active')
-        return "MULTIPLE OF 3"
-    }
-}
-
-function sevenMultiple(number) {
-    let node = document.querySelector('.multSeven')
-    const calc = number % 7
-
-    if(calc !== 0){
-        node.classList.remove('active')
-        return "NOT MULTIPLE OF 7"
-    } else{
-        node.classList.add('active')
-        return "MULTIPLE OF 7"
-    }
+    
+    calc === 0 ? node.classList.add('active') : node.classList.remove('active')
+    
 }
